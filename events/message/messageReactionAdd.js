d53b1d1a09389ab1d40d8f4708cf4d5596131fc3
DistroByte <jamesthackett1@gmail.com>
2022-10-14 18:20:17 +0100

    Adds message role reaction events for various roles.

---
+++ events/message/messageReactionAdd.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: "messageReactionAdd",
+  async execute(reaction, user) {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    // Contributer role
+    if (reaction.message.id == "1027831065139761164") {
+      if (reaction.emoji.name === "✅") {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.add("1027828366474235984");
+      }
+    }
+
+    // Documentor role
+    if (reaction.message.id == "1027831134173802537") {
+      if (reaction.emoji.name === "✅") {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.add("1027828648121729024");
+      }
+    }
+
+    // Developer role
+    if (reaction.message.id == "1027831265711378452") {
+      if (reaction.emoji.name === "✅") {
+        await reaction.message.guild.members.cache
+          .get(user.id)
+          .roles.add("1027828559970062336");
+      }
+    }
+  }
+}
